c91fdd14b66aea712cd279afa8e7205a88d626ee
mathiasfuhr <mathiasgilvanf@gmail.com>
2024-12-20 16:05:39 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.dominio;
+
+public class Anime {
+    private String tipo;
+    private int episodios;
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public int getEpisodios() {
+        return episodios;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Dconstrutores.test;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+    }
+}
